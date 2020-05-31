0e5e2011f52d7aaf42cb598765aa3afbee1ce5e7
elizabethLomb <elizabeth.lombardi7@gmail.com>
2020-05-31 16:53:29 +0200

    Utils removed import

--- phone-catalogue-redux/src/Utils/index.js
+++ phone-catalogue-redux/src/Utils/index.js
@@ -1,6 +1,5 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import checkPropTypes from 'check-prop-types';
 
 export const enzymeSetUp = () => {
   configure({ adapter: new Adapter() });
